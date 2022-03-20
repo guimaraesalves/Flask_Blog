aa2948142eeec9e9904513e44dc8ebe1ed793b2f
guimaraesalves <guimaraesalvesmateus@gmail.com>
2022-03-20 14:41:32 -0300

    commit init create portifolio

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Mateus Guimarães Alves',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'Março 20, 2022'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Mateus Guimarães Alves',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'Março 21, 2022'
     }
 ]
 
